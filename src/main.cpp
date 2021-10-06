f5a1f6b85287e65eaeda21b3c6bb88867b5c2c81
bork <bork.andrew.ryan@gmail.com>
2021-10-05 21:08:01 -0700

    fdasfads

--- src/main.cpp
+++ src/main.cpp
@@ -10,17 +10,17 @@ int main(){
     mpu6050::set_gyro_set(mpu6050::gyro_range::deg_1000);
     mpu6050::set_clk(mpu6050::clk::x_gyro);
     mpu6050::set_fsync(mpu6050::fsync::input_dis);
-    mpu6050::set_dlpf_bandwidth(mpu6050::dlpf::hz_184);
+    mpu6050::set_dlpf_bandwidth(mpu6050::dlpf::hz_21);
     mpu6050::wake_up();
     mpu6050::print_debug();
     float x,y,z;
-    int data[6];
-    int s_data[6];
+    float data[6];
+    float s_data[6];
     for(int i = 0; i<6;i++) s_data[i]=0;
     int n = 1000;
     int i = n;
     while(i--){
-        mpu6050::read_raw(data);
+        mpu6050::read(data);
         std::cout<< data[0] << " " << data[1] << " " << data[2] << " " << data[3] << " " << data[4] << " " << data[5] << "\n";
         for(int i = 0; i < 6; i++)s_data[i]+=data[i];
         usleep(1000);
