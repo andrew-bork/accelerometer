b14dad56626478939e3cd7b04f1522697a6f85f2
bork <bork.andrew.ryan@gmail.com>
2021-10-08 16:04:34 -0700

    dsafdsa

--- test/calib.cpp
+++ test/calib.cpp
@@ -10,7 +10,7 @@ int main(){
     mpu6050::set_gyro_set(mpu6050::gyro_range::deg_250);
     mpu6050::set_clk(mpu6050::clk::x_gyro);
     mpu6050::set_fsync(mpu6050::fsync::input_dis);
-    mpu6050::set_dlpf_bandwidth(mpu6050::dlpf::hz_10);
+    mpu6050::set_dlpf_bandwidth(mpu6050::dlpf::hz_5);
     mpu6050::wake_up();
     mpu6050::print_debug();
     
