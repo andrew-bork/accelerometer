0aa1479bac8090fdec2acf6bcbf00222bf6490e2
bork <bork.andrew.ryan@gmail.com>
2021-10-09 14:00:03 -0700

    fdsafds

--- test/read.cpp
+++ test/read.cpp
@@ -39,10 +39,10 @@ int main(){
 
     mpu6050::init();
     mpu6050::set_accl_set(mpu6050::accl_range::g_2);
-    mpu6050::set_gyro_set(mpu6050::gyro_range::deg_250);
+    mpu6050::set_gyro_set(mpu6050::gyro_range::deg_1000);
     mpu6050::set_clk(mpu6050::clk::y_gyro);
     mpu6050::set_fsync(mpu6050::fsync::input_dis);
-    mpu6050::set_dlpf_bandwidth(mpu6050::dlpf::hz_260);
+    mpu6050::set_dlpf_bandwidth(mpu6050::dlpf::hz_5);
     mpu6050::wake_up();
 
     mpu6050::set_offsets(1360, -158, -1345, -199, 138, 72);
