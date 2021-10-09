b1ce94153767bd06cb2a2704258c1c9d0d647de3
bork <bork.andrew.ryan@gmail.com>
2021-10-09 14:00:55 -0700

    fsafdsa

--- test/read.cpp
+++ test/read.cpp
@@ -39,7 +39,7 @@ int main(){
 
     mpu6050::init();
     mpu6050::set_accl_set(mpu6050::accl_range::g_2);
-    mpu6050::set_gyro_set(mpu6050::gyro_range::deg_1000);
+    mpu6050::set_gyro_set(mpu6050::gyro_range::deg_500);
     mpu6050::set_clk(mpu6050::clk::y_gyro);
     mpu6050::set_fsync(mpu6050::fsync::input_dis);
     mpu6050::set_dlpf_bandwidth(mpu6050::dlpf::hz_5);
