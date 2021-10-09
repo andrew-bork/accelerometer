2a5ec3db467a671330e41ba5332b0e7b91174084
bork <bork.andrew.ryan@gmail.com>
2021-10-09 11:58:59 -0700

    fdsafdsa

---
+++ test/client.cpp
@@ -0,0 +1,20 @@
+#include <socket.h>
+#include <cstdio>
+#include <cstring>
+
+int main(){
+
+    sock::socket client(sock::unix, sock::tcp);
+    if(client.fd < 0){
+        return -1;
+    }
+
+    sock::socket::un_connection unix_connection = client.un_connect("/run/test");
+    char * buf[1024];
+
+    unix_connection.read(buf, 1024);
+    printf("%s\n", buf);
+
+
+
+}
